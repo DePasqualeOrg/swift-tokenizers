9542a2ea12d72590997436fe6d80af056b442271
Anthony DePasquale <anthony@depasquale.org>
2026-02-18 21:11:32 +0100

    Rename package to swift-tokenizers

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-transformers",
+    name: "swift-tokenizers",
     platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         .library(name: "Tokenizers", targets: ["Tokenizers"]),
