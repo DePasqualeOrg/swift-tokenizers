89c6f44abd01d00ba59d14c19f8472241f8bc836
Anthony DePasquale <anthony@depasquale.org>
2026-04-06 02:55:42 +0200

    Use published TokenizersRust artifact

--- TokenizersRustBinary/Package.swift
+++ TokenizersRustBinary/Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TokenizersRust",
-            path: "../Binaries/TokenizersRust.xcframework"
+            url: "https://github.com/DePasqualeOrg/swift-tokenizers/releases/download/tokenizers-rust-rename-rust-artifact-1/TokenizersRust-main-3.xcframework.zip",
+            checksum: "ec56924f0ed8493937da06496f09a680ed81803fd20ef33769538a95ec85ff77"
         )
     ]
 )
